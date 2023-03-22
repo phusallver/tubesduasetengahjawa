f7644059929820cacace06bd6449df7a219f8a9d
phusallver <103794258+phusallver@users.noreply.github.com>
2023-03-22 11:48:00 +0700

    inisialisasi tubes

---
+++ src/main/java/com/mycompany/tubesduasetengahjawa/Tubesduasetengahjawa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tubesduasetengahjawa;
+
+/**
+ *
+ * @author USER
+ */
+public class Tubesduasetengahjawa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
